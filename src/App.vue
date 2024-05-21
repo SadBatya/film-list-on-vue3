90fe5d47a40dfb52741f279d929173060c39169c
sadbatya <sadbatya@mail.ru>
2024-05-21 09:20:49 -0400

    finish project

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,54 @@
-<script setup>
-  import Navigation from './components/Navigation.vue'
-</script>
-
 <template>
-  <Navigation />
-  <router-view></router-view>
+  <div class="person_list">
+    <ListItem v-for="(person, index) in database" :key='index' :name="person.name" :id="person.id" :salary="person.salary" v-on:deleteItem="deleteCard"/>
+  </div>
 </template>
 
-<style scoped>
 
+<script setup>
+  import ListItem from "./components/ListItem.vue";
+  import {ref} from 'vue'
+
+  let database = ref([
+    {
+      id: 1,
+      name: 'Vladimir',
+      salary: '35000'
+    },
+    {
+      id:2,
+      name: 'Nikolay',
+      salary: '3000'
+    },
+    {
+      id: 3,
+      name: 'Kostya',
+      salary: '55000'
+    }
+  ])
+
+  function deleteCard(id) {
+    database.value =  database.value.filter(person => person.id != id)
+  }
+</script>
+
+
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+.person_list{
+  display: flex;
+  flex-direction: column;
+}
 </style>
---
+++ src/components/ListItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="person">
+    name: {{ name }} salary: {{ salary }} id: {{ id }}
+    <button @click="$emit('deleteItem', id)">Delete person</button>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  name: String,
+  salary: String,
+  id: Number
+});
+</script>
+
+<style>
+.person {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 10px;
+}
+
+</style>
